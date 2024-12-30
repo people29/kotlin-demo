45b6da301a5026952ca1de0a46fe66bb8220ae0f
Mシ <m@odds.team>
2024-12-31 00:43:08 +0700

    remove investor and sa module

--- src/main/kotlin/com/spring/demo/investor/InvestorRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.spring.demo.investor
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface InvestorRepository : JpaRepository<Investor, Long> {
-    fun findByCardNo(cardNo: String): Investor?
-}
